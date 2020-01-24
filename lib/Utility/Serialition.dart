e5d0d328c7bd91d29ddcf7d882f1646d912561b1
vinayakusername <gpraut88@gmail.com>
2020-01-24 15:50:53 +0530

    updated

--- lib/Utility/Serialition.dart
+++ lib/Utility/Serialition.dart
@@ -6,7 +6,7 @@ import 'dart:convert';
 
 class Serialition
 {
- 
+  
 Future<String> loadUserInfo() async {
   
    //It load's the user data from JSON file.
@@ -15,7 +15,7 @@ Future<String> loadUserInfo() async {
    
    //usersList1 is a  reference variable of type UsersList model class and it store's object of UsersList.
    //UsersList.fromJson is named constructor in which we are passing decoded data of json file.
-   UsersList usersList1 = UsersList.fromJson(jsonResponse);
+  UsersList usersList1 = UsersList.fromJson(jsonResponse);
     
  // It returns the object of UsersList.
     //return usersList1;
